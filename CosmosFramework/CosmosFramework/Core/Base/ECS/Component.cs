a9e74d528842ba38da73ad858c34e10d7145eee8
Don <zhengdonn@gmail.com>
2020-09-01 16:57:02 +0800

    Update：迁移文件结构，更新behaviour接口

---
+++ CosmosFramework/CosmosFramework/Core/Base/ECS/Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos
+{
+    public class Component:Object
+    {
+        Component parent;
+        public Component Parent { get { return parent; } set { parent = value; } }
+    }
+}
