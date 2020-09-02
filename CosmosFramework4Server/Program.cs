a0e37e3ce590f6da8cd2d7ec027f407872351fce
Don <zhengdonn@gmail.com>
2020-09-02 17:46:40 +0800

    Update：规范扩展module方法名

--- CosmosFramework4Server/Program.cs
+++ CosmosFramework4Server/Program.cs
@@ -29,7 +29,7 @@ namespace CosmosFramework4Server
             Utility.Debug.SetHelper(new ConsoleDebugHelper());
             Utility.Debug.LogInfo("Server Start Running !");
             GameManager.NetworkManager.Connect(ip, port, System.Net.Sockets.ProtocolType.Udp);
-            GameManager.External.GetExternalModule<PeerManager>();
+            GameManager.External.GetModule<PeerManager>();
             Task.Run(GameManagerAgent.Instance.OnRefresh);
             while (true) { }
          //   Task.Run(AsyncCoroutine.Instance.Start);
